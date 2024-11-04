6c2ea094e889dff1aeda5b755061729813f400a3
caldeira04 <joao.p.caldeira@icloud.com>
2024-11-03 21:03:23 -0300

    added readme

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,12 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = {}
-  }
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = {
+			ensure_installed = { "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
+			auto_install = true,
+			highlight = {
+				enable = true,
+			},
+		},
+	},
 }
