80809ae70c093cf33350fb691eadc3433a5cd7e3
caldeira04 <joao.p.caldeira@icloud.com>
2025-05-22 11:19:43 -0300

    automated dev commit

--- lua/plugins/nvim-ts-autotag.lua
+++ lua/plugins/nvim-ts-autotag.lua
@@ -1,3 +1,10 @@
 return {
 	"windwp/nvim-ts-autotag",
+  require("nvim-ts-autotag").setup ({
+    opts = {
+      enable_close = true,
+      enable_rename = true,
+      enable_close_on_slash = true,
+    }
+  })
 }
