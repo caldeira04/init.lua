111d8d51cd25c72badbf39ec18e1cf6f62d5ba6f
caldeira04 <joao.p.caldeira@icloud.com>
2025-05-27 09:02:18 -0300

    feat: added screenkey

--- lua/plugins/simple.lua
+++ lua/plugins/simple.lua
@@ -2,4 +2,9 @@ return {
 	{ "numToStr/Comment.nvim", opts = {} },
 	{ "norcalli/nvim-colorizer.lua" },
 	{ "wakatime/vim-wakatime", lazy = false },
+	{
+		"NStefan002/screenkey.nvim",
+		lazy = false,
+		version = "*", -- or branch = "main", to use the latest commit
+	},
 }
