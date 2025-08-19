feb7317c2519d8fa85de7b2be6373d0b20bad652
caldeira04 <joao.p.caldeira@icloud.com>
2025-08-19 12:53:23 -0300

    adj: tabstop, removed inline diagnostics

--- init.lua
+++ init.lua
@@ -2,73 +2,73 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
 vim.pack.add({
-	{ src = "https://github.com/neovim/nvim-lspconfig" },
-	{ src = "https://github.com/mason-org/mason.nvim" },
-	{ src = "https://github.com/mason-org/mason-lspconfig.nvim" },
-	{ src = "https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim" },
-	{ src = "https://github.com/L3MON4D3/LuaSnip" },
-	{ src = "https://github.com/rafamadriz/friendly-snippets" },
-	{ src = "https://github.com/Saghen/blink.cmp" },
-	{ src = "https://github.com/supermaven-inc/supermaven-nvim" },
-	{ src = "https://github.com/rose-pine/neovim",                         name = "rose-pine" },
-	{ src = "https://github.com/windwp/nvim-autopairs" },
-	{ src = "https://github.com/windwp/nvim-ts-autotag" }
+    { src = "https://github.com/neovim/nvim-lspconfig" },
+    { src = "https://github.com/mason-org/mason.nvim" },
+    { src = "https://github.com/mason-org/mason-lspconfig.nvim" },
+    { src = "https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim" },
+    { src = "https://github.com/L3MON4D3/LuaSnip" },
+    { src = "https://github.com/rafamadriz/friendly-snippets" },
+    { src = "https://github.com/Saghen/blink.cmp" },
+    { src = "https://github.com/supermaven-inc/supermaven-nvim" },
+    { src = "https://github.com/rose-pine/neovim",                         name = "rose-pine" },
+    { src = "https://github.com/windwp/nvim-autopairs" },
+    { src = "https://github.com/windwp/nvim-ts-autotag" }
 })
 
 require("nvim-autopairs").setup()
 require("nvim-ts-autotag").setup({
-	opts = {
-		enable_close = true,
-		enable_rename = true,
-		enable_close_on_slash = true,
-	}
+    opts = {
+        enable_close = true,
+        enable_rename = true,
+        enable_close_on_slash = true,
+    }
 })
 require("supermaven-nvim").setup({})
 require("mason").setup()
 require("mason-lspconfig").setup()
 require("mason-tool-installer").setup({
-	ensure_installed = {
-		"lua_ls",
-		"stylua",
-		"ts_ls"
-	},
+    ensure_installed = {
+        "lua_ls",
+        "stylua",
+        "ts_ls"
+    },
 })
 
 vim.lsp.config("lua_ls", {
-	settings = {
-		Lua = {
-			runtime = {
-				version = "LuaJIT",
-			},
-			diagnostics = {
-				globals = {
-					"vim",
-					"require",
-				},
-			},
-			workspace = {
-				library = vim.api.nvim_get_runtime_file("", true),
-			},
-			telemetry = {
-				enable = false,
-			},
-		},
-	},
+    settings = {
+        Lua = {
+            runtime = {
+                version = "LuaJIT",
+            },
+            diagnostics = {
+                globals = {
+                    "vim",
+                    "require",
+                },
+            },
+            workspace = {
+                library = vim.api.nvim_get_runtime_file("", true),
+            },
+            telemetry = {
+                enable = false,
+            },
+        },
+    },
 })
 
 require("luasnip.loaders.from_vscode").lazy_load()
 require("blink.cmp").setup({
-	signature = { enabled = true },
-	completion = {
-		documentation = { auto_show = true, auto_show_delay_ms = 500 },
-		menu = {
-			auto_show = true,
-			draw = {
-				treesitter = { "lsp" },
-				columns = { { "kind_icon", "label", "label_description", gap = 1 }, { "kind" } },
-			},
-		},
-	},
+    signature = { enabled = true },
+    completion = {
+        documentation = { auto_show = true, auto_show_delay_ms = 500 },
+        menu = {
+            auto_show = true,
+            draw = {
+                treesitter = { "lsp" },
+                columns = { { "kind_icon", "label", "label_description", gap = 1 }, { "kind" } },
+            },
+        },
+    },
 })
 
 vim.cmd [[colorscheme rose-pine]]
@@ -90,54 +90,51 @@ vim.keymap.set("n", "<C-j>", "<C-w><C-j>")
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>")
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<leader>f", function()
-	vim.lsp.buf.format({ async = true })
+    vim.lsp.buf.format({ async = true })
 end)
 
 vim.keymap.set("n", "[d", function()
-	vim.diagnostic.jump({
-		count = 1,
-		float = true
-	})
+    vim.diagnostic.jump({
+        count = 1,
+        float = true
+    })
 end)
 vim.keymap.set("n", "]d", function()
-	vim.diagnostic.jump({
-		count = -1,
-		float = true
-	})
+    vim.diagnostic.jump({
+        count = -1,
+        float = true
+    })
 end)
 
 -- opts
 
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.smartindent = true
+vim.opt.expandtab = true
+vim.opt.tabstop = 1
+vim.opt.softtabstop = 1
+vim.opt.shiftwidth = 4
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
-
-vim.diagnostic.config({
-	virtual_lines = true,
-})
 
 vim.o.winborder = "rounded"
 
 local autocmd = vim.api.nvim_create_autocmd
 
 autocmd("TextYankPost", {
-	desc = "Highlight when yanking (copying) text",
-	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-	callback = function()
-		vim.highlight.on_yank()
-	end,
+    desc = "Highlight when yanking (copying) text",
+    group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+    callback = function()
+        vim.highlight.on_yank()
+    end,
 })
 
 autocmd("BufWritePre", {
-	pattern = "*",
-	desc = "Auto-format on save",
-	callback = function()
-		vim.lsp.buf.format({
-			async = false,
-		})
-	end,
+    pattern = "*",
+    desc = "Auto-format on save",
+    callback = function()
+        vim.lsp.buf.format({
+            async = false,
+        })
+    end,
 })
