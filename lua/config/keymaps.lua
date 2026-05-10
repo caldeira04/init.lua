b4dfb138226e6af14d261cf18a4f003c9540e77d
caldeira04 <joao.p.caldeira@icloud.com>
2026-05-10 19:09:31 -0300

    add descriptions

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,18 +1,20 @@
 local map = vim.keymap.set
 local fff = require('fff')
 
-map("n", "<Esc>", "<cmd>nohlsearch<CR>")
-map("n", "<leader>pv", ":Ex<CR>")
-map("n", "J", "mzJ`z")
+map("n", "<Esc>", "<cmd>nohlsearch<CR>", { desc = "Removes highlight after searching" })
+map("n", "<leader>pv", ":Ex<CR>", { desc = "Opens file explorer" })
+map("n", "J", "mzJ`z", { desc = "Pulls bottom line up" })
 map("i", "<C-c>", "<Esc>")
 
-map("v", "J", ":m '>+1<CR>gv=gv")
-map("v", "K", ":m '<-2<CR>gv=gv")
+map("v", "J", ":m '>+1<CR>gv=gv", { desc = "Moves selection one line down" })
+map("v", "K", ":m '<-2<CR>gv=gv", { desc = "Moves selection one line up" })
 
+-- copypaste
 map("x", "<leader>p", [["_dP]])
 map({ "n", "v" }, "<leader>y", [["+Y]])
 map({ "n", "v" }, "<leader>d", '"_d')
 
+-- move between splits
 map("n", "<C-h>", "<C-w><C-h>")
 map("n", "<C-l>", "<C-w><C-l>")
 map("n", "<C-j>", "<C-w><C-j>")
@@ -20,30 +22,30 @@ map("n", "<C-k>", "<C-w><C-k>")
 
 map("n", "<leader>f", function()
     vim.lsp.buf.format({ async = true })
-end)
+end, { desc = "Format file" })
 
-map("n", "[d", function() vim.diagnostic.jump({ count = -1, float = true }) end)
-map("n", "]d", function() vim.diagnostic.jump({ count = 1, float = true }) end)
+map("n", "[d", function() vim.diagnostic.jump({ count = -1, float = true }) end, { desc = "Jump to previous diagnostic" })
+map("n", "]d", function() vim.diagnostic.jump({ count = 1, float = true }) end, { desc = "Jump to next diagnostic" })
 
 
 map("n", '<leader>ff', function()
     fff.find_files()
-end)
+end, { desc = "Open interactive file finder" })
 
 map("n", '<leader>fg', function()
     fff.live_grep()
-end)
+end, { desc = "Open interactive grep search" })
 
 map("n", '<leader>bf', function()
     fff.buffers()
-end)
+end, { desc = "Open buffer selection" })
 
 map("n", '<leader>co', function()
     fff.colors()
-end)
+end, { desc = "Open theme selector" })
 
 map("n", '<leader>u', function()
     vim.cmd("Undotree")
-end)
-map("n", 'gr', vim.lsp.buf.references)
-map("n", 'gi', vim.lsp.buf.implementation)
+end, { desc = "Open undotree" })
+map("n", 'gr', vim.lsp.buf.references, { desc = "Open references of function in project" })
+map("n", 'gi', vim.lsp.buf.implementation, { desc = "Open implementation of function on project" })
